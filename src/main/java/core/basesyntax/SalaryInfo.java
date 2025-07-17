c45862dfa54b70f6c27c9bef548168811ae11e6b
Vladyslav Makoda <vlad.makoda@gmail.com>
2025-07-17 21:26:36 +0200

    fixed styles and tests passed

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,36 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+
+    public String getSalaryInfo(String[] names, String[] data,
+                                String dateFrom, String dateTo) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate dateF = LocalDate.parse(dateFrom, formatter);
+        LocalDate dateT = LocalDate.parse(dateTo, formatter);
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Report for period ").append(dateFrom)
+                .append(" - ").append(dateTo);
+
+        for (String name : names) {
+            Integer earning = 0;
+            for (String row: data) {
+                String[] rowParts = row.split(" ");
+
+                if (rowParts[1].equals(name)) {
+                    LocalDate workDate = LocalDate.parse(rowParts[0], formatter);
+
+                    if (workDate.isAfter(dateF.minusDays(1))
+                            && workDate.isBefore(dateT.plusDays(1))) {
+                        earning += Integer.parseInt(rowParts[2]) * Integer.parseInt(rowParts[3]);
+                    }
+                }
+            }
+            stringBuilder.append("\n").append(name).append(" - ").append(earning);
+        }
+        return stringBuilder.toString();
     }
 }
